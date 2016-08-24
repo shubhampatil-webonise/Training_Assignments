29fa0c91218640566df7c21b5d01f8523412c424
shubhampatil-webonise <shubham.patil@weboniselab.com>
2016-08-24 20:18:36 +0530

    add month selection and flight booking feature

--- QA/QA_Automation/automation_testing.py
+++ QA/QA_Automation/automation_testing.py
@@ -3,19 +3,54 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from datetime import datetime
 
+from unittest_data_provider import data_provider
 
 import unittest
 import random
 import time
+import csv
+
+
+def csvParser():
+	super_tuple = ()
+
+	with open('data.csv', 'rb') as csvfile:
+		reader = csv.reader(csvfile)
+
+		for row in reader:
+			data_tuple = ()
+
+			for data in row:
+				data_tuple = data_tuple + (data.strip(),)
+
+			super_tuple = super_tuple + (data_tuple,)
+
+	#return super_tuple
+
+	return (('25/8/2016', '26/8/2016', '1'),)
+
 
 class AutomationTesting(unittest.TestCase):
-	def setUp(self):
-		self.driver = webdriver.Firefox()
 
-	def test_automation(self):
+	data_under_test = lambda : csvParser()
+
+
+	def setUp(self):
+		pass
+
+
+	@data_provider(data_under_test)
+	def test_automation(self, checkin_date, checkout_date, number_of_travellers):
+
+		print checkin_date
+		print checkout_date
+		print number_of_travellers
 		
+		self.driver = webdriver.Firefox()		
 		self.driver.get("https://www.tripadvisor.in")
+
 
 		flights_tab_button = self.driver.find_element_by_id('rdoFlights')
 		flights_tab_button.click()
@@ -42,22 +77,58 @@ class AutomationTesting(unittest.TestCase):
 		destination_choice_selected = destination_choice_dropdown.find_element_by_class_name('autocompleter-selected')
 		destination_choice_selected.click()
 
-		#element = self.driver.find_element_by_id('checkIn')
-		#element.click()
+		checkin = self.driver.find_element_by_id('metaCheckInSpan')
+		checkin.click()
+
+		calendar = self.driver.find_element_by_class_name('calendar')
+
+		nav_prev_handler = calendar.find_element_by_xpath(".//span[@class = 'prev']/a")
+		nav_next_handler = calendar.find_element_by_xpath(".//span[@class = 'next']/a")
 
 
-		#element = self.driver.find_element_by_id('checkOut')
-		#element.click()
+		checkin_date = datetime.strptime(checkin_date, '%d/%m/%Y').date()
+		print checkin_date
 
-		number_of_travellers = self.driver.find_element_by_id('fadults')
-		number_of_travellers.click()
+		current_checkin_date = checkin.find_element_by_id('checkIn').get_attribute('value')
+		#current_checkin_date = datetime.strptime("28/2/2017", '%d/%m/%Y').date()
+		current_checkin_date = datetime.strptime(current_checkin_date, '%d/%m/%Y').date()
+		print current_checkin_date
 
-		options = number_of_travellers.find_elements_by_tag_name('option')
+		print current_checkin_date.month - checkin_date.month
+
+		if checkin_date > current_checkin_date:
+			month_diff = (checkin_date.year - current_checkin_date.year)*12 + (checkin_date.month - checkin_date.month)
+
+			for x in range(month_diff):
+				nav_next_handler.click()
+
+		else:
+			month_diff = (current_checkin_date.year - checkin_date.year)*12 + (current_checkin_date.month - checkin_date.month)
+
+			for x in range(month_diff):
+				nav_prev_handler.click()
+
+	
+		'''
+		#checkout = self.driver.find_element_by_id('metaCheckOutSpan')
+		#checkout.click()
+
+		#calendar = self.driver.find_element_by_class_name('calendar')
+
+		#nav_prev_handler = calendar.find_element_by_xpath(".//span[@class = 'prev']/a")
+		#nav_next_handler = calendar.find_element_by_xpath(".//span[@class = 'next']/a")
+
+	
+		travellers = self.driver.find_element_by_id('fadults')
+		travellers.click()
+
+		options = travellers.find_elements_by_tag_name('option')
 
 		for option in options:
-			if option.get_attribute('value') == "2": #user_value
-				option.click()
+			if option.get_attribute('value') == str(number_of_travellers): #user_value
+				option.click()'''
 
+		'''
 		self.driver.execute_script("window.scrollTo(0,0)")
 
 		search_button = self.driver.find_element_by_id('SUBMIT_FLIGHTS')
@@ -77,7 +148,7 @@ class AutomationTesting(unittest.TestCase):
 
 
 		WebDriverWait(self.driver, 10).until(
-				lambda driver: len(self.driver.window_handles) > 1
+				lambda : len(self.driver.window_handles) > 1
 			)
 
 
@@ -104,74 +175,38 @@ class AutomationTesting(unittest.TestCase):
 			EC.invisibility_of_element_located((By.XPATH, "//div[@id = 'loadingOverlay']"))
 		)
 
-		elements = WebDriverWait(self.driver, 10).until(
-			EC.presence_of_all_elements_located((By.XPATH, "//div[@id = 'taplc_flight_list_0']/descendant::div[@class = 'flightList']/div[@class = 'entry show']//div[@class='mainButton']"))
+		flights = WebDriverWait(self.driver, 20).until(
+			EC.presence_of_all_elements_located((By.XPATH, "//div[@id = 'taplc_flight_list_0']//div[@class = 'flightList']/div[@class = 'entry show']"))#//div[.//*[contains(text(), 'Musafir.com')]]//div[@class='mainButton']"))
 		)
 
-		print len(elements)
 
-		''' 
+		flights = self.driver.find_elements_by_xpath("//div[@id = 'taplc_flight_list_0']//div[@class = 'flightList']/div[@class = 'entry show']")
+		random_flight = random.choice(flights)
 
-		flight_booking_buttons = self.driver.find_elements_by_xpath("//div[@id = 'taplc_flight_list_0']/descendant::div[@class = 'flightList']/div[@class = 'entry show']//div[@class='mainButton']")
+		self.driver.execute_script("return arguments[0].scrollIntoView();", random_flight)
+		self.driver.execute_script("window.scrollBy(0, -150);")
 
-
-		#random_flight_button = random.choice(flight_booking_buttons)
+		flight_booking_button = random_flight.find_element_by_xpath(".//div[@class = 'purchaseLinkColumn']//div[@class = 'mainButton']")
 		
+		amount_of_booking = random_flight.find_element_by_xpath(".//div[@class = 'purchaseLinkColumn']//span[@class = 'price']").text
+		airline_name = random_flight.find_element_by_xpath(".//div[@class = 'flightInfoColumn']//div[@class = 'airlineName']").text
+
+		flight_booking_button.click()
+		self.driver.switch_to_window(self.driver.window_handles[0])
+
+		print "Flight Details  ======================== "
+		print "Airline Name :", airline_name
+		print "Flight : ", amount_of_booking
+		print "=========================================== "'''
 		
-		button = WebDriverWait(self.driver, 10).until(
-				EC.element_to_be_clickable((By.XPATH, "//div[@id = 'taplc_flight_list_0']/descendant::div[@class = 'flightList']/div[@class = 'entry show']//div[@class='mainButton']"))
-			)
+		#self.driver.close()
 
 
+	def tearDown(self):
+		pass
 
-		button.click()'''
-
-		'''flight_list = WebDriverWait(self.driver, 10).until(
-				EC.visibility_of_element_located((By.XPATH, "//div[@id = 'taplc_flight_list_0']/descendant::div[@class = 'flightList']"))
-			)
-
-		print flight_list.text'''
-
-
-		'''flight_list = WebDriverWait(self.driver, 10).until(
-				##EC.presence_of_all_elements_located((By.XPATH, "//div[@id = 'taplc_flight_list_0']/descendant::div[@class = 'flightList']/div[@class = 'entry show']"))
-
-			)'''
-
-		#print "total flights ==================" , len(flight_list)
-
-		'''random_flight = random.choice(flight_list)
-
-		print "Flight ===================", random_flight.text
-
-		flight_booking_button = random_flight.find_element_by_xpath(".//descendant::div[@class = 'purchaseLinkColumn']/a[1]")#/div[@class = 'mainButton']")
-
-		print "==========================", flight_booking_button.text
-		flight_booking_button.click()'''
-
-		#flight_booking_button = WebDriverWait(self.driver, 10).until(
-		#		EC.element_to_be_clickable((By.XPATH, "//div[@class = 'purchaseLinkColumn']/a/div[@class = 'mainButton']"))
-		#	)		
-
-		#flight_booking_button.click()
-
-		#flight_booking_button = random_flight.find_element_by_xpath("//div[@class = 'purchaseLinkColumn']/a/div[@class = 'mainButton']")
-		#flight_booking_button.click()
-
-
-		'''flight_list = self.driver.find_elements_by_xpath("//div[@id = 'taplc_flight_list_0']/descendant::div[@class = 'flightList']/div[@class = 'entry show']")
-		random_flight = random.choice(flight_list)
-
-		flight_booking_button = random_flight.find_element_by_xpath("//div[@class = 'purchaseLinkColumn']/a/div[@class = 'mainButton']")
-		print flight_booking_button.get_attribute('class')
-		#flight_booking_button.click()'''
-
-
-
-
-	#def tearDown(self):
-	#	self.driver.close()
 
 
 if __name__ == "__main__":
+	#AutomationTesting.number_of_travellers = 4
 	unittest.main()
